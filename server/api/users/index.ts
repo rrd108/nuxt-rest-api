16286a4fa63f7ab7597ce12a240d7d453f741cb2
rrd108 <rrd@webmania.cc>
2024-02-21 20:29:25 +0100

    users endpoint implemented

---
+++ server/api/users/index.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async event => {
+  if (event.req.method != 'GET') {
+    throw createError({ status: 405, message: 'Method not Allowed' })
+  }
+
+  const token = getHeader(event, 'token')
+
+  const user = await prisma.users.findFirst({ where: { token } })
+  if (!user) {
+    throw createError({ status: 401, message: 'Authentication error' })
+  }
+
+  const users = await prisma.users.findMany()
+  return users
+})
