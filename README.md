02df66f3fad9bdd99dc3b3e20271e87bbc24e199
rrd108 <rrd@webmania.cc>
2024-02-21 20:13:43 +0100

    prisma and md5 added to implement login

--- README.md
+++ README.md
@@ -16,3 +16,15 @@
 1. `npx nuxi@latest init nuxt-rest-api`
 2. `yarn`
 3. `yarn dev`
+4. `yarn add -D prisma`
+5. `npx prisma init`
+6. `npx prisma db pull`
+7. `npx prisma generate`
+8. `yarn add md5`
+
+`npx prisma migrate dev --name init`
+
+## Notes
+
+`const id = getRouterParam(event, 'id')`
+CORS: https://www.youtube.com/watch?v=g6BVCBkRelw&t=849s
--- server/api/users/login.ts
+++ server/api/users/login.ts
@@ -1,10 +1,19 @@
-export default defineEventHandler(event => {
+import { PrismaClient } from '@prisma/client'
+import md5 from 'md5'
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async event => {
   console.log('event', event)
   if (event.req.method != 'POST') {
     throw createError({ status: 405, message: 'Method not Allowed' })
   }
 
-  return {
-    user: 'rrd',
+  const data = await readBody(event)
+
+  const user = await prisma.users.findUnique({ where: { email: data.email, password: md5(data.password) } })
+  if (!user) {
+    throw createError({ status: 401, message: 'User can not be identified' })
   }
+  delete user.password
+  return user
 })
