4d4156f55996183b8aaf89b0ea542f992498fc93
rrd108 <rrd@webmania.cc>
2024-02-21 20:53:05 +0100

    POST products endpoint implemented

--- server/api/products/index.ts
+++ server/api/products/index.ts
@@ -2,10 +2,17 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async event => {
-  if (event.req.method != 'GET') {
-    throw createError({ status: 405, message: 'Method not Allowed' })
+  if (event.req.method == 'GET') {
+    const products = await prisma.products.findMany()
+    return products
   }
 
-  const products = await prisma.products.findMany()
-  return products
+  if (event.req.method == 'POST') {
+    const data = await readBody(event)
+    const product = await prisma.products.create({ data: data })
+    if (!product) {
+      throw createError({ status: 401, message: 'Product creation error' })
+    }
+    return product
+  }
 })
